4823c1dca18739124d4e05c19284e2022bada75e
le-champion_t-ext <thibaut.lechampion-ext@vyv3.fr>
2022-11-16 15:55:31 +0100

    Modification accès BDD

--- index.php
+++ index.php
@@ -5,4 +5,16 @@
     print($db_connection);
     //$result = pg_query($db_connection, "SELECT lastname FROM employees");
     print("Hello<BR/>");
+
+    $dsn = "pgsql:host=svxazpgsql01.postgres.database.azure.com;port=5432;dbname=test_cms;user=poc_cms;password=AP\kKb>t(HiTZoc.?Gdb";
+   
+    try{
+       $conn = new PDO($dsn);
+       
+       if($conn){
+        echo "Connecté à $dbname avec succès!";
+       }
+    }catch (PDOException $e){
+       echo $e->getMessage();
+    }
 ?>
