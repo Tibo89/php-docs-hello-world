99cf30217d18e7de4abeb79d24057959b0e26cc1
le-champion_t-ext <thibaut.lechampion-ext@vyv3.fr>
2022-11-16 17:08:28 +0100

    Modification accès BDD

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
     echo "Hello World!<BR/>";
-    echo "Coucou<BR/>";
+    echo "Coucou tout le monde !<BR/>";
     $db_connection = pg_connect("host=svxazpgsql01.postgres.database.azure.com dbname=test_cms@svxazpgsql01 sslmode=require user=poc_cms password=AP\kKb>t(HiTZoc.?Gdb");
     print($db_connection);
     //$result = pg_query($db_connection, "SELECT lastname FROM employees");
