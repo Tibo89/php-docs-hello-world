767b39ce412fa226031325a5e9ef41642f18657f
le-champion_t-ext <thibaut.lechampion-ext@vyv3.fr>
2022-11-16 15:39:30 +0100

    Modification accès BDD

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
     echo "Hello World!<BR/>";
     echo "Coucou<BR/>";
-    $db_connection = pg_connect("host=svxazpgsql01.postgres.database.azure.com dbname=test_cms user=poc_cms password=AP\kKb>t(HiTZoc.?Gdb");
+    $db_connection = pg_connect("host=svxazpgsql01.postgres.database.azure.com dbname=test_cms@svxazpgsql01 sslmode=require user=poc_cms password=AP\kKb>t(HiTZoc.?Gdb");
     print($db_connection);
     //$result = pg_query($db_connection, "SELECT lastname FROM employees");
     print("Hello<BR/>");
