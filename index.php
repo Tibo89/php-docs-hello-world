d32cb1f8cc38bb9dbf7b0a3afc0f73fe25feaf4e
le-champion_t-ext <thibaut.lechampion-ext@vyv3.fr>
2022-11-17 11:56:37 +0100

    Mise à jour

--- index.php
+++ index.php
@@ -1,31 +1,27 @@
 <?php 
-    echo "Hello World v2!<BR/>";
-    echo "Coucou tout le monde !<BR/>";
-    print("Hello<BR/>");
+   echo "Hello World v (quarante douze)!<BR/>";
+   echo "Coucou tout le monde !<BR/>";
+   print("Début du script<BR/><BR/>");
 
-
-
-
-    $dsn = "pgsql:host=svxazpgsql01.postgres.database.azure.com;port=5432;sslmode=require;dbname=test_cms;user=poc_cms;password=Soleil@49100!";
+   $dsn = "pgsql:host=svxazpgsql01.postgres.database.azure.com;port=5432;sslmode=require;dbname=test_cms;user=poc_cms;password=Soleil@49100!";
    
-    try{
-       $conn = new PDO($dsn);
-       
-       if($conn){
-        echo "Connecté à $dbname avec succès!<BR/>";
-        $sql = 'SELECT * FROM test_schema.test_table';
-        foreach ($conn->query($sql) as $row) {
-           print $row['Nom'];
-           print $row['Prenom'] . "<BR/>";
-        }
-     
-       }
-       else{
-         echo "Erreur à $dbname !<BR/>";
-       }
-    }catch (PDOException $e){
-       echo $e->getMessage();
-    }
+   try{
+      $conn = new PDO($dsn);
 
-    print("Ola<BR/>");
+      if($conn){
+         echo "Connecté à $dbname avec succès ! Contenu de la table : <BR/><BR/>";
+         $sql = 'SELECT * FROM test_schema.test_table';
+         foreach ($conn->query($sql) as $row) {
+            print $row['Prenom'] . " " . $row['Nom'] . "<BR/>";
+         }
+      }
+      else{
+         echo "Erreur à $dbname !<BR/>";
+      }
+   }
+   catch (PDOException $e){
+      echo $e->getMessage();
+   }
+
+   print("<BR/>Fin du script<BR/>");
 ?>
