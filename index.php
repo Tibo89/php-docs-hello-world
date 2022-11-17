7543ceba56cdcc0d58c6574eb5dbe9faead785b7
le-champion_t-ext <thibaut.lechampion-ext@vyv3.fr>
2022-11-17 11:52:40 +0100

    Mise à jour

--- index.php
+++ index.php
@@ -1,32 +1,7 @@
 <?php 
     echo "Hello World v2!<BR/>";
     echo "Coucou tout le monde !<BR/>";
-    $db_connection = pg_connect("host=svxazpgsql01.postgres.database.azure.com dbname=test_cms@svxazpgsql01 sslmode=require user=poc_cms password=Soleil@49100!");
-    print($db_connection);
-    //$result = pg_query($db_connection, "SELECT lastname FROM employees");
     print("Hello<BR/>");
-
-    if (!$db_connection) {
-      echo "An error occurred.\n";
-      exit;
-    }
-    
-    $result = pg_query($db_connection, "SELECT * FROM test_schema.test_table;");
-    if (!$result) {
-      echo "<br />An error occurred.<br />";
-      exit;
-    }
-
-    if ($result->num_rows == 0) {
-      echo "<br />0 results<br />";
-    }
-    
-    while ($row = pg_fetch_row($result)) {
-      echo "NOM: $row[1]  Prenom : $row[2]";
-      echo "<br />";
-    }
-
-    $db_connection->close();
 
 
 
@@ -45,9 +20,12 @@
         }
      
        }
+       else{
+         echo "Erreur à $dbname !<BR/>";
+       }
     }catch (PDOException $e){
        echo $e->getMessage();
     }
 
-
+    print("Ola<BR/>");
 ?>
