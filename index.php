39ff285427e15693a470be6970bb8625e866ac9b
le-champion_t-ext <thibaut.lechampion-ext@vyv3.fr>
2022-11-17 11:42:38 +0100

    Mise à jour

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
     echo "Hello World v2!<BR/>";
     echo "Coucou tout le monde !<BR/>";
-    $db_connection = pg_connect("host=svxazpgsql01.postgres.database.azure.com dbname=test_cms@svxazpgsql01 sslmode=require user=poc_cms password=AP\kKb>t(HiTZoc.?Gdb");
+    $db_connection = pg_connect("host=svxazpgsql01.postgres.database.azure.com dbname=test_cms@svxazpgsql01 sslmode=require user=poc_cms password=Soleil@49100!");
     print($db_connection);
     //$result = pg_query($db_connection, "SELECT lastname FROM employees");
     print("Hello<BR/>");
