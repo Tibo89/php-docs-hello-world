e7cc810731b503181f45c086cdd6fbba52e74b5c
le-champion_t-ext <thibaut.lechampion-ext@vyv3.fr>
2022-11-17 11:26:02 +0100

    Mise à jour

--- index.php
+++ index.php
@@ -31,4 +31,18 @@
     }catch (PDOException $e){
        echo $e->getMessage();
     }
+
+
+   $sql = "SELECT * FROM test_schema.test_table";
+   $result = $conn->query($sql);
+
+   if ($result->num_rows > 0) {
+   // output data of each row
+   while($row = $result->fetch_assoc()) {
+      echo "id: " . $row["ID"]. " - Name: " . $row["Nom"]. " " . $row["Prenom"]. "<br>";
+   }
+   } else {
+   echo "0 results";
+   }
+   $conn->close();
 ?>
