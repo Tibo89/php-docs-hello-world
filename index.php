789ef06ffb7d6098eda274873f0d6dd9038f36ad
le-champion_t-ext <thibaut.lechampion-ext@vyv3.fr>
2022-11-17 09:04:29 +0100

    Mise à jour

--- index.php
+++ index.php
@@ -17,4 +17,16 @@
     }catch (PDOException $e){
        echo $e->getMessage();
     }
+
+    $dsn = "pgsql:host=svxazpgsql01.postgres.database.azure.com;port=5432;dbname=test_cms;user=poc_cms;password=AP\kKb>t(HiTZoc.?Gdb";
+   
+    try{
+       $conn = new PDO($dsn);
+       
+       if($conn){
+        echo "Connecté à $dbname avec succès!";
+       }
+    }catch (PDOException $e){
+       echo $e->getMessage();
+    }
 ?>
